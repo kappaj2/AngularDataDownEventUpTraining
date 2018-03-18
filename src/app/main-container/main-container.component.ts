11d00d3bfd874298fa9077731d6a3c1d570213e9
Andre <kappaj@gmail.com>
2018-03-18 20:02:45 +0200

    Initial commit

---
+++ src/app/main-container/main-container.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-container',
+  templateUrl: './main-container.component.html',
+  styleUrls: ['./main-container.component.css']
+})
+export class MainContainerComponent implements OnInit {
+
+  names = ['John','Ben','Peter'];
+  name:string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  eventFromOuter(passed:string){
+    this.name = passed;
+  }
+}
